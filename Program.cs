ef06777e29aed482803061cb6a76558029f5b593
gnegovetic <gnegovetic@hotmail.com>
2020-09-21 21:53:56 -0700

    Removed unused 'using'

--- Program.cs
+++ Program.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ConsoleCmdProc;
+﻿using ConsoleCmdProc;
 using iText.Kernel.Pdf;
+using System;
+using System.IO;
 
 
 namespace PdfPasswordProtect
