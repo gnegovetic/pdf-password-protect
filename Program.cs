ee8e7517d88c8e6e728c6bfd2a4bb352a4fe5f02
gnegovetic <gnegovetic@hotmail.com>
2020-09-22 12:21:14 -0700

    Added checking for correct cmd arguments.

--- Program.cs
+++ Program.cs
@@ -15,28 +15,30 @@ namespace PdfPasswordProtect
             cl.DefineOption("input", "Input File", true, false);
             cl.DefineOption("pw", "Password", true, false);
 
-            cl.LoadArguments(args);
+            var suc = cl.LoadArguments(args);
+            if (suc)
+            {
+                string inputFile = cl.GetOptionValue("input")[0];
+                string password = cl.GetOptionValue("pw")[0];
 
-            string inputFile = cl.GetOptionValue("input")[0];
-            string password = cl.GetOptionValue("pw")[0];
 
+                // Code below copied from https://itextpdf.com/en/demos/protect-pdf-files-free-online
+                string OutputFolder = Path.GetDirectoryName(inputFile);
+                byte[] USERPASS = System.Text.Encoding.Default.GetBytes(password);
+                byte[] OWNERPASS = System.Text.Encoding.Default.GetBytes(password);
 
-            // Code below copied from https://itextpdf.com/en/demos/protect-pdf-files-free-online
-            string OUTPUT_FOLDER = Path.GetDirectoryName(inputFile);
-            byte[] USERPASS = System.Text.Encoding.Default.GetBytes(password);
-            byte[] OWNERPASS = System.Text.Encoding.Default.GetBytes(password);
+                PdfReader pdfReader = new PdfReader(inputFile);
 
-            PdfReader pdfReader = new PdfReader(inputFile);
+                WriterProperties writerProperties = new WriterProperties();
+                writerProperties.SetStandardEncryption(USERPASS, OWNERPASS, EncryptionConstants.ALLOW_PRINTING, EncryptionConstants.ENCRYPTION_AES_128);
 
-            WriterProperties writerProperties = new WriterProperties();
-            writerProperties.SetStandardEncryption(USERPASS, OWNERPASS, EncryptionConstants.ALLOW_PRINTING, EncryptionConstants.ENCRYPTION_AES_128);
+                string outputFile = Path.Combine(OutputFolder, Path.GetFileNameWithoutExtension(inputFile) + "_Protected.pdf");
+                PdfWriter pdfWriter = new PdfWriter(new FileStream(outputFile, FileMode.Create), writerProperties);
+                PdfDocument pdfDocument = new PdfDocument(pdfReader, pdfWriter);
+                pdfDocument.Close();
 
-            string outputFile = Path.Combine(OUTPUT_FOLDER, Path.GetFileNameWithoutExtension(inputFile) + "_Protected.pdf");
-            PdfWriter pdfWriter = new PdfWriter(new FileStream(outputFile, FileMode.Create), writerProperties);
-            PdfDocument pdfDocument = new PdfDocument(pdfReader, pdfWriter);
-            pdfDocument.Close();
-
-            Console.WriteLine("Create file: " + outputFile);
+                Console.WriteLine("Create file: " + outputFile);
+            }
         }
 
      }
